c464964fb6f372fb0a7d7d73e2087e5b2126b22c
ITJacob <jacob.wd8816@gmail.com>
2021-11-07 19:52:48 +0800

    feat: add message

--- control/MessageCenter.ts
+++ control/MessageCenter.ts
@@ -1,7 +1,15 @@
 class MessageCenter {
   private pip: [];
-  emit(message: { type: string; content: any }) {}
-  subscribe(type: string) {}
+  private target = new EventTarget();
+
+  emit(type: string, detail: any) {
+    const e = new CustomEvent(type, { detail });
+    this.target.dispatchEvent(e);
+  }
+
+  subscribe(type: string) {
+    this.target.addEventListener;
+  }
 }
 
 const mc = new MessageCenter();
