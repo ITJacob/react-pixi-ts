2559f7c7f181fbd2ad98ebf4892ab60ccaa616e6
ITJacob <jacob.wd8816@gmail.com>
2021-11-17 09:39:25 +0800

    feat: add skill

--- view/UiManager.ts
+++ view/UiManager.ts
@@ -5,25 +5,51 @@ export class UiManager {
   app: PIXI.Application;
   skills: Button[] = [];
   heros: Button[] = [];
+  enemies: Button[] = [];
   constructor(app: PIXI.Application) {
     this.app = app;
     Button.init(this.app.renderer);
   }
 
   init() {
-    ['火弹', '火球', '焰爆', '陨石'].forEach((text, i) => {
+    ['恶魔1', '恶魔2', '恶魔3'].forEach((text, i) => {
       const btn = new Button(this.app.stage);
-      btn.x = 300;
-      btn.y = 560 + i * 60;
+      btn.x = 80 + i * 100;
+      btn.y = 120;
       btn.text = text;
-      this.skills[i] = btn;
+      this.enemies[i] = btn;
     });
-    ['炎魔', '卫队长', '医官'].forEach((text, i) => {
+
+    ['恶魔4', '恶魔5'].forEach((text, i) => {
       const btn = new Button(this.app.stage);
-      btn.x = 40;
-      btn.y = 600 + i * 80;
+      btn.x = 120 + i * 100;
+      btn.y = 180;
+      btn.text = text;
+      this.enemies[i + 3] = btn;
+    });
+
+    ['医官', '卫队长'].forEach((text, i) => {
+      const btn = new Button(this.app.stage);
+      btn.x = 120 + i * 100;
+      btn.y = 420;
       btn.text = text;
       this.heros[i] = btn;
+    });
+
+    ['炎魔', '刺客', '法师'].forEach((text, i) => {
+      const btn = new Button(this.app.stage);
+      btn.x = 80 + i * 100;
+      btn.y = 480;
+      btn.text = text;
+      this.heros[i] = btn;
+    });
+
+    ['火弹', '火球', '焰爆', '陨石'].forEach((text, i) => {
+      const btn = new Button(this.app.stage);
+      btn.x = 60 + i * 80;
+      btn.y = 720;
+      btn.text = text;
+      this.skills[i] = btn;
     });
   }
 }
